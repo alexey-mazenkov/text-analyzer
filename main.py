7b89ecf5d391631de7c098ab0a97e11be050c9ab
Andrey <andreiliza1981@mail.ru>
2019-11-16 16:20:48 +0700

    language check

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 # Developers: A.Mazenkov
 #             K.Kravtsov
 #             A.Mikhailov
-
+import dictionaries
 import analysis as al
 
 # TODO(Mkhlva): Здесь должна быть проверка того, на каком языке введён текст.
 text = input()
+cnt = len(text)
+for vs in dictionaries.vowels_ru:
+    text = text.replace(vs, '')
+if len(text) == cnt:
+    print('Язык: en')
+else:
+    print('Язык: ru')
+
 
 
 
